f884474abf3ec02349e5bc9992637a0a0fe0dbba
wallacce135 <wallacce135@gmail.com>
2021-02-14 22:58:40 +0300

    14.02.2021

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea ullam a explicabo. Atque voluptates quod repellat. Sunt debitis odit sequi optio voluptas facere quos fuga! Numquam id laudantium perspiciatis tempora.</p>
   </div>
 </template>
+<style scoped>
+.about{
+  font-family: 'Gilroy';
+}
+</style>
