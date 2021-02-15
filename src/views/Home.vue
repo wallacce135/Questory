b149199d73978b2ca29a5b23be9f493a5a8b59cc
Revilise <nice.mutnykh@mail.ru>
2021-02-15 20:01:01 +0300

    nothing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="login">
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<style scoped>
+  .login {
+    background-image: url('/img/greenBackground.png');
   }
-}
-</script>
+</style>
